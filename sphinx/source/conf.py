736ee43597bc09be848be69af381441cd7010703
slaclau <seb.laclau@gmail.com>
2024-07-17 08:21:08 +0100

    Include images in sphinx examples

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -85,5 +85,6 @@ with open(file, "a+", encoding="utf-8") as f:
         f.write(".. literalinclude:: ../../src/plotly_gtk/demo.py\n")
         f.write(f"   :start-after: = \"{demo}\n")
         f.write("   :end-before: el\n\n")
+        f.write(f".. image:: examples/{demo}.png\n\n")
         
         
