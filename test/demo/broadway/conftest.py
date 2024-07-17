456b83b196a5f1bcf668a290282c7819b5c839e4
slaclau <seb.laclau@gmail.com>
2024-07-17 23:46:09 +0100

    test: update tests in demo/broadway
    Ensure STDERR is empty

--- test/demo/broadway/conftest.py
+++ test/demo/broadway/conftest.py
@@ -2,17 +2,11 @@ import pathlib
 import subprocess
 
 
-def pytest_runtest_setup(item):
-    print(f"pytest_runtest_setup({item}")
-
-
 def pytest_configure(config):
-    print("Configure")
     subprocess.run(["pkill", "gtk4-broadwayd"])
     subprocess.Popen(["/usr/bin/gtk4-broadwayd", ":5"])
     (pathlib.Path(__file__).parent / "results").mkdir(parents=True, exist_ok=True)
 
 
 def pytest_unconfigure(config):
-    print("Unconfigure")
     subprocess.run(["pkill", "gtk4-broadwayd"])
