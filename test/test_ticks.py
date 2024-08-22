6d77c43c0c012719dc1b6ce6de24645a6d95fb3d
slaclau <seb.laclau@gmail.com>
2024-08-22 20:14:01 +0100

    fix: fix log ticks

---
+++ test/test_ticks.py
@@ -0,0 +1,32 @@
+class TestTicks:
+    def test_tick_first(self):
+
+        assert False
+
+    def test_tick_increment(self):
+
+        assert False
+
+    def test_calculate(self):
+
+        assert False
+
+    def test_prepare(self):
+
+        assert False
+
+    def test_round_up(self):
+
+        assert False
+
+    def test_round_dtick(self):
+
+        assert False
+
+    def test_auto_ticks(self):
+
+        assert False
+
+    def test_array_ticks(self):
+
+        assert False
