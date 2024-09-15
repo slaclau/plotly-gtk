a8a5adcf746644880758856bb1f7d55759b85cb4
slaclau <seb.laclau@gmail.com>
2024-09-15 15:20:22 +0100

    test(ticks): initial work

--- test/test_ticks.py
+++ test/test_ticks.py
@@ -1,10 +1,24 @@
-class TestTicks:
-    def test_tick_first(self):
+import pytest
+from plotly_gtk.utils import get_base_fig, update_dict
+from plotly_gtk.utils.ticks import Ticks
 
-        assert False
+base_fig = get_base_fig()
+base_fig = update_dict(base_fig, base_fig["layout"]["template"])
+
+
+class TestBasic:
+    @pytest.fixture()
+    def ticks_object(self):
+        update = {"xaxis": {"_range": [0, 100], "_type": "linear", "nticks": 0}}
+        fig = update_dict(base_fig["layout"], update)
+        rtn = Ticks(fig, "xaxis", 100)
+        rtn.prepare()
+        return rtn
+
+    def test_tick_first(self, ticks_object):
+        assert ticks_object.tick_first() == 0
 
     def test_tick_increment(self):
-
         assert False
 
     def test_calculate(self):
